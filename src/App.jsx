8c9deadd9428d2d72c6f4e8b5b6301407f7dde5c
Julio Henrique <juliohjesus@gmail.com>
2022-04-20 19:03:57 -0300

    feat: connecting with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { StartPage } from './components/StartPage'
 import { QuestionPage } from './components/QuestionPage'
 
 import './App.css'
+// const url = "https://opentdb.com/api.php?amount=5&type=multiple"
 
 function App() {
   const [startPageState, setStartPageState] = useState(true)
-
+  const [questionsData, setQuestionsData] = useState([])
+  
   function startPageToFalse() {
     setStartPageState(prev => !prev)
   }
+  
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+      .then(res => res.json())
+      .then(data => setQuestionsData(data.results))
+  }, [])
 
   return (
     <>
       {
         startPageState
           ? <StartPage pageToFalse={startPageToFalse} />
-          : <QuestionPage />
+          : <QuestionPage questionsData={questionsData} />
       }
     </>
   )
--- src/components/QuestionPage/index.jsx
+++ src/components/QuestionPage/index.jsx
@@ -1,4 +1,27 @@
-export function QuestionPage() {
+export function QuestionPage({ questionsData }) {
+    const showQuestions = questionsData.map(item => {
+        // const { question,  }
+        /* 
+        category: "Entertainment: Video Games"
+correct_answer: "GLaDOS"
+difficulty: "easy"
+incorrect_answers: (3) ['Chell', 'Wheatley', 'Rick']
+question: "Who is the main antagonist in the Portal franchise?"
+        */
+        return (
+            <>
+            <h2 className="" >How would one say goodbye in Spanish?</h2>
+                <div className="answers--section" >
+                <p className="answers answer--selected" >Adiós</p>
+                <p className="answers" >Hola</p>
+                <p className="answers" >Au Revoir</p>
+                <p className="answers" >Salir</p>
+                </div>
+            </>
+        )
+    })
+    console.log(questionsData)
+
     return (
         <section className="container">
             <div className="questions--card">
@@ -9,27 +32,7 @@ export function QuestionPage() {
                     <p className="answers" >Au Revoir</p>
                     <p className="answers" >Salir</p>
                 </div>
-            </div>
-            <div className="separator" />
-            <div className="questions--card">
-                <h2 className="" >Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h2>
-                <div className="answers--section" >
-                    <p className="answers answer--selected" >Cabbage Patch Kids</p>
-                    <p className="answers" >Transformers</p>
-                    <p className="answers" >Care Bears</p>
-                    <p className="answers" >Rubik’s Cube</p>
-                </div>
-            </div>
-            <div className="separator" />
-            <div className="questions--card">
-                <h2 className="" >How would one say goodbye in Spanish?</h2>
-                <div className="answers--section" >
-                    <p className="answers" >Adiós</p>
-                    <p className="answers" >Hola</p>
-                    <p className="answers" >Au Revoir</p>
-                    <p className="answers answer--selected" >Salir</p>
-                </div>
-            </div>
+            </div>            
             <div className="separator" />
             <div className="questions--card">
                 <h2 className="" >Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h2>
@@ -40,19 +43,10 @@ export function QuestionPage() {
                     <p className="answers" >Rubik’s Cube</p>
                 </div>
             </div>
-            <div className="separator" />
-            <div className="questions--card">
-                <h2 className="" >How would one say goodbye in Spanish?</h2>
-                <div className="answers--section" >
-                    <p className="answers" >Adiós</p>
-                    <p className="answers answer--selected" >Hola</p>
-                    <p className="answers" >Au Revoir</p>
-                    <p className="answers" >Salir</p>
-                </div>
-            </div>
+            
             <div className="separator" />
 
-            <button>Check answers</button>
+            {true ? <button>Check answers</button> : <button>Play Again</button>}
         </section>
         
     )
