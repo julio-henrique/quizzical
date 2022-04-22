1e9da6a8f00be51d1e708e2a9fade2921050629a
Julio Henrique <juliohjesus@gmail.com>
2022-04-22 19:56:57 -0300

    feat: useEffect improved + apiData state

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,61 @@ import { useEffect, useState } from 'react'
 import { StartPage } from './components/StartPage'
 import { QuestionPage } from './components/QuestionPage'
 
+import { nanoid } from 'nanoid'
+
 import './App.css'
 // const url = "https://opentdb.com/api.php?amount=5&type=multiple"
 
 function App() {
   const [startPageState, setStartPageState] = useState(true)
-  const [questionsData, setQuestionsData] = useState([])
+  const [apiData, setApiData] = useState([])
   
   function startPageToFalse() {
     setStartPageState(prev => !prev)
   }
+
+  // The API is encoded, base64, here I'm decoding.
+  function b64_to_utf8( str ) {
+    return decodeURIComponent((window.atob( str )));
+  }
+  fetch('https://opentdb.com/api.php?amount=5&category=31&type=multiple&encode=base64')
   
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5&category=31&type=multiple&encode=base64')
-    // https://opentdb.com/api.php?amount=5&category=31
-      .then(res => res.json())
-      .then(data => setQuestionsData(data.results))
-  }, [])
-  
-  console.log(questionsData)
+    .then(res => res.json())
+      .then(data => {
+        setApiData(() => {
+          let newArray = []
+
+          for (let i = 0; i < data.results.length; i++) {
+            newArray.push({
+              id: nanoid(),
+              question: b64_to_utf8(data.results[i].question),
+              correct_answer: b64_to_utf8(data.results[i].correct_answer),
+              
+              alternatives: [
+                ...data.results[i].incorrect_answers.map(incorrect => b64_to_utf8(incorrect)),
+                b64_to_utf8(data.results[i].correct_answer)
+              ].sort(() => Math.random() - 0.5),
+              
+              select_answer: '',
+              isCorrect: false
+            })
+          }
+
+          return newArray
+        })
+      })
+  }, []) 
+
+  // console.log(apiData) 
 
   return (
     <>
       {
         startPageState
           ? <StartPage pageToFalse={startPageToFalse} />
-          : <QuestionPage questionsData={questionsData} />
+          : <QuestionPage apiData={apiData} />
       }
     </>
   )
--- src/components/QuestionPage/index.jsx
+++ src/components/QuestionPage/index.jsx
@@ -1,33 +1,36 @@
 import { nanoid } from 'nanoid'
 
-export function QuestionPage({ questionsData }) {
-    const showQuestions = questionsData.map((item, index) => {
-        const { question, incorrect_answers, correct_answer } = item
+export function QuestionPage({ apiData }) {
 
-        const id = nanoid()
-
-        // The API is encoded, base64, here I'm decoding.
-        function b64_to_utf8( str ) {
-            return decodeURIComponent((window.atob( str )));
-        }
+    const showQuestions = apiData.map((item) => {
+        // Destructuring item
+        const { question, alternatives, id  } = item
+        
+        
+        // maping and showing the alternitves
+        const showAlternatives = alternatives.map((itemAlternatives, ) => {
+            
+            // console.log(id)
+            return (    
+                <div 
+                    className="answers" 
+                >
+                    {itemAlternatives}
+                </div>
+            )
+        })
 
         return (
             <>
-            <div key={id}>
-                <h2 className="">{b64_to_utf8(question)}</h2>
+                <h2 key={id} >{question}</h2>
                     <div className="answers--section" >
-                    <p className="answers" >{b64_to_utf8(incorrect_answers[2])}</p>
-                    <p className="answers" >{b64_to_utf8(incorrect_answers[0])}</p>
-                    <p className="answers" >{b64_to_utf8(incorrect_answers[1])}</p>
-                    <p className="answers" >{b64_to_utf8(correct_answer)}</p>
+
+                            {showAlternatives}
                     </div>
                 <div className="separator" />
-            </div>
-
             </>
         )
     })
-    console.log(questionsData)
 
     return (
         <section className="container">
@@ -36,8 +39,8 @@ export function QuestionPage({ questionsData }) {
             </div>
 
             {
-                true 
-                    ? <button>Check answers</button> 
+                true
+                    ? <button  >Check answers</button> 
                     : <button>Play Again</button>
             }
         </section>
